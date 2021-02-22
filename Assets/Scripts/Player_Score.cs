efe75cacc27d7280c37294e6717e62a63a9857c8
ADD415 <ADAVID212@caledonian.ac.uk>
2021-02-22 13:38:18 +0000

    Time and Score added

---
+++ Assets/Scripts/Player_Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Score : MonoBehaviour
+{
+    private float time = 0;
+    public int playerScore = 0;
+    public GameObject timeUI;
+    public GameObject playerScoreUI;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeUI.gameObject.GetComponent<Text>().text = ("Time: " + (int)time);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore);
+    }
+
+    void OnTriggerEnter2D (Collider2D trig)
+    {
+
+        if (trig.gameObject.name == "Object")
+        {
+            playerScore += 10;
+            Destroy(trig.gameObject);
+        }
+    }
+}  
